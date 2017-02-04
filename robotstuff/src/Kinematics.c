e962e909bf0963d6ae260cda5f910ee6cff69344
Billy Sullivan <wsullivan@wpi.edu>
2017-02-04 16:23:21 -0500

    Fixed Kinematics.c

--- robotstuff/src/Kinematics.c
+++ robotstuff/src/Kinematics.c
@@ -13,14 +13,16 @@
 // variables are in inches, should be in mm
 double link1 = 6;
 double link2 = 5.5;
-double xOffset = 5.625;
+double yOffset = 5.625;
 
 
 double * getPos(int theta1, int theta2)
+		
 {
-		static double  point[2];
-		point[0] = cos(theta1)*link1 + cos(theta2)*link2 + xOffset; 	//Calculate X position
-		point[1] = sin(theta1)*link1 + sin(theta2)*link2;				//Calculate Y position
+		static double point[2];
+		 
+		point[0] = cos(theta1*(3.14/180))*link1 + cos((theta1 + theta2)*(3.14/180))*link2;  				//Calculate X position
+		point[1] = sin(theta1*(3.14/180))*link1 + sin((theta1 + theta2)*(3.14/180))*link2 + yOffset;		//Calculate Y position
 		
 		return point; //Return array containing the X Y position of the end of arm 
 		
