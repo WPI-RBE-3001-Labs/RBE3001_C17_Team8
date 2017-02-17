fb7a78df4d2eba3c7fbdfed700afd69451668a27
jplombardi <jplombardi@wpi.edu>
2017-02-17 15:35:36 -0500

    Added if not defined and end if for making the compiler happy

--- robotstuff/src/Kinematics.h
+++ robotstuff/src/Kinematics.h
@@ -5,7 +5,11 @@
  *      Author: Billy Sullivan
  */
 
+#ifndef KINEMATICS_H_
+#define KINEMATICS_H_
 
 double degToRad(double ang);
 double * getPos(int theta1, int theta2);
 
+
+#endif /* KINEMATICS_H_ */
