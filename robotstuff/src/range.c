b76dc2829a4349efe85e725bf2a9a1c2b9670554
jplombardi <jplombardi@wpi.edu>
2017-02-22 16:45:01 -0500

    added ranging functions

---
+++ robotstuff/src/range.c
@@ -0,0 +1,11 @@
+#include "RBELib/RBELib.h" //RBELib
+#include "range.h"
+
+//unsigned char getRange(void){
+//	//function returns distance in inches.
+//	int ir = getADC(4);
+//	if(ir < 400){return 0;}
+//	else{ir = ir*-0.1333 + 93.333; return ir;}
+//}
+
+
---
+++ robotstuff/src/range.h
@@ -0,0 +1,7 @@
+
+#ifndef RANGE_H_
+#define RANGE_H_
+
+//int getRange(void);
+
+#endif /* RANGE_H_ */
