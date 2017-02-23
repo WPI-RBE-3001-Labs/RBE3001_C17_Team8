218d6a8f22495ab0a22fe7f72502e26a54600998
jplombardi <jplombardi@wpi.edu>
2017-02-22 22:35:26 -0500

    Should be working with a linearization of small part of range

--- robotstuff/src/range.c
+++ robotstuff/src/range.c
@@ -1,9 +1,9 @@
 #include "RBELib/RBELib.h" //RBELib
 #include "range.h"
 
-//unsigned char getRange(void){
-//	//function returns distance in inches.
-//	int ir = getADC(4);
-//	if(ir < 400){return 0;}
-//	else{ir = ir*-0.1333 + 93.333; return ir;}
-//}
+unsigned char getRange(void){
+	//function returns distance in tenths of inches.
+	int ir = getADC(4);
+	if(ir < 400){return 0;}
+	else{ir = ir*-0.1333 + 93.333; return ir;}
+}
--- robotstuff/src/range.h
+++ robotstuff/src/range.h
@@ -2,6 +2,6 @@
 #ifndef RANGE_H_
 #define RANGE_H_
 
-//int getRange(void);
+unsigned char getRange(void);
 
 #endif /* RANGE_H_ */
