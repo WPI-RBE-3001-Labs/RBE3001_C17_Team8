84aa016a82cc5f490f131727a3f51797385ae03a
jplombardi <jplombardi@wpi.edu>
2017-02-27 20:31:46 -0500

    Commented more and made aesthetic, no functional change

--- robotstuff/src/range.c
+++ robotstuff/src/range.c
@@ -9,23 +9,18 @@
 //}
 unsigned int getRange(void){
 	//function returns distance in tenths of inches.
-	int ir = getADC(4);
+	int ir = getADC(4);//pick up the data in the fourth ADC slot
 
-	if(ir < 400)
+	if(ir < 400)//If it is reading that there is nothing there, print code 42
+	{return 42;}//code 42 means there is no data
+
+	else //otherwise...
 	{
-		return 42;
-	}
-	else
-	{
-		ir = ir*-0.1333 + 93.333;
-		if(ir < 29 && ir >= 0)
-		{
-			return ir;
-		}
+		ir = ir*-0.1333 + 93.333;//follow linearized model of sensor data
+		if(ir < 29 && ir >= 0)//remove unecessary data
+		{return ir;}//return a worthwhile value
+
 		else
-		{
-			return 0;
-		}
-
+		{return 0;}//return a good-enough value because data is wrong
 	}
 }
