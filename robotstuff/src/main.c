42cde989a25cfd4d59dc65cd69a5f0e67c74368d
jplombardi <jplombardi@wpi.edu>
2017-02-27 20:33:16 -0500

    Main function trimmed of impertinent data, commented heavily

--- robotstuff/src/main.c
+++ robotstuff/src/main.c
@@ -4,6 +4,8 @@
  *      Authors: Joe Lombardi, Billy Sullivan, William Godsey
  */
 
+//A project at Worcester Polytechnic Institute in partial fulfillment of
+//the requirements of the course Unified Robotics III - Manipulation
 
 #include "RBELib/RBELib.h"
 #include "Kinematics.h"
@@ -11,64 +13,44 @@
 #include "range.h"
 #include "math.h"
 
-
-
-ISR(TIMER0_OVF_vect){tot_overflow++;
-}//count up tot_overflow on timer0 ovf interrupt
+ISR(TIMER0_OVF_vect){tot_overflow++;}//count up tot_overflow on timer0 ovf interrupt
 
 int main(void){
-	int state = 0;
+	int state = 0;//keeps track of the state of the function
 
 	float yHeight = -1;//Height of the block, set to 0 due to kinematics from link0
 	float xOffset = 6.8;//Distance from arm to belt
 	float xDist;//will hold distance of block x
 
-	char done = 0;
-	unsigned int timeCycles = 0;
-	unsigned int timeToGrip = 1400;
-	unsigned int timeToCloseGrip = 1000;
-	unsigned int timeToWeigh = 500;
-	unsigned int timeToGo = 5000;
+	char done = 0;//a very important variable that has a self-explanatory use later
 
+	//Note, conversion can be done as 1 tick ~ 1/100 of a second.
+	unsigned int timeCycles = 0;//integer to hold total time elapsed after reset
+	unsigned int timeToGrip = 1400;//constant integer to compare against for delay
+	unsigned int timeToCloseGrip = 1000;//same as above, for different use
+	unsigned int timeToWeigh = 500;//short delay for gripper to close
+	unsigned int timeToGo = 5000;//time it takes to move to
 
-	double theta1 =0;
-	double theta2=0;
-	int waitTheta2=0;
-	int waitTheta1=0;
+	double theta1 = 0;//declare we are using two doubles for out working theta values
+	double theta2 = 0;
+	int waitTheta2 = 0;//declare we are using two integer values for later thetas
+	int waitTheta1 = 0;
 
-	DDRBbits._P4 = OUTPUT;
-	initADC(4);
-
-	armInitialization();
+	armInitialization(); //call initialization functions, found in auxiliary.c
 	initializeButtons(); //call initialization functions
 
-	setServo(0,0);
-	setServo(1,255);
+	setServo(0,0);//set the gripper to open initially
+	setServo(1,255);//set the conveyor to move forward, stay like that for duration
 
-	while(1){//Check buttons
+	while(1){
 
-		//printf("%6.2f\n\r", getRange()*.1);
-
-//		xDist = xOffset + (((float)getRange())/10) + 0.5;
-//		theta1 = getAngles(xDist, yHeight)[0];//Set theta 1 lower link
-//		theta2 = getAngles(xDist, yHeight)[1];//Set theta 2 upper link
-//		printf("%i, %i, %f\n\r", (int)theta1, (int)theta2, xDist);
-//
-//		if(tot_overflow>2){
-//		gotoAngles((int)theta1,(int)theta2+15);
-//		tot_overflow = 0;
-//		}
-		//gotoAngles(0,0);
-
-
-
-		switch(state){
+		switch(state){//keep in state format, easier to troubleshoot and track flow
 
 		case 0://Basically poll IR for new information, compare when recieved
-				if(getRange() != 42){
+				if(getRange() != 42){//while printing code 42, do nothing
 					state = 1;
-					done = 0;
-					timeCycles = 0;
+					done = 0;//make sure important variables are in correct standing
+					timeCycles = 0;//reset time, in case this is second call of function
 					printf("\r\n");//newline
 					printf("block detected\r\n");
 					while(tot_overflow<200){}//delay a bit
@@ -80,52 +62,47 @@ int main(void){
 			//Y position will remain constant,
 			//X position will change only a little bit.
 			if(getRange() != 42){
-			xDist = xOffset + (((float)getRange())/10) + 0.5;
+			xDist = xOffset + (((float)getRange())/10) + 0.5;//math out the block dstnce
 			theta1 = getAngles(xDist, yHeight)[0];//Set theta 1 lower link
 			theta2 = getAngles(xDist, yHeight)[1];//Set theta 2 upper link
 			printf("blocking\r");
 			}
-			else{done = 1;}
+			else{done = 1;}//realize that the block is done blocking the sensor
 
-			if(tot_overflow > 2){
+			if(tot_overflow > 2){//100Hz PID loop
 				gotoAngles((int)theta1,(int)theta2);
 			}
 
-			if(done){state = 2;
-			waitTheta1 = potAngle(2);
-			waitTheta2 = potAngle(3);
-			printf("\r\n");
-			printf("angles are, (%d,%d)\r\n",waitTheta1,waitTheta2);
-			printf("waiting\r\n");
-
+			if(done){//if the block IS done blocking the sensor,
+				state = 2;//move to next state
+				waitTheta1 = potAngle(2);//store angles
+				waitTheta2 = potAngle(3);//as direct read of the configuration
+				printf("\r\n");
+				printf("angles are, (%d,%d)\r\n",waitTheta1,waitTheta2);
+				printf("waiting\r\n");
 			}//move to next state
 			break;
 
 		case 2://Now wait for block to be in place, go to a starting position.
-			if(tot_overflow>2){
+			if(tot_overflow>2){//PID loop
 			gotoAngles(waitTheta1,(waitTheta2+15));//holding position
 			tot_overflow = 0;//reset PID overflow loop
 
-			if(timeCycles < timeToGrip){
-				timeCycles++;
-				printf(". . .\r");
-			}
-			else{
-				state = 3;
-				printf("\r\n");
-				printf("BLOCK IN RANGE\r\n");
-			}
-
+				if(timeCycles < timeToGrip){//check if enough time has elapsed
+					timeCycles++;//increment time to delay
+					printf(". . .\r");
+				}
+				else{
+					state = 3;//move to grab state
+					printf("\r\n");
+					printf("BLOCK IN RANGE\r\n");
+				}
 			}
 			break;
-//
-//			//FIGURE OUT HOW TO WAIT UNTIL BLOCK IN PLACE
-//		//	state = 3;
-//			break;
-//
-		case 3://Go to the thing, and then...
-			if(tot_overflow>2){
-			gotoAngles(waitTheta1,waitTheta2);
+
+		case 3://Go to the configuration, and then...
+			if(tot_overflow>2){//PID timed loop.
+			gotoAngles(waitTheta1,waitTheta2);//Move to pick up configuration
 			}
 
 			//continuation condition, and initialization
@@ -136,16 +113,16 @@ int main(void){
 
 				printf("GRIP NOW!!!!\r\n");
 
-				char grip = 1;
-				timeCycles = 0;
-				while(grip){
+				char grip = 1;//set char as grip, will tell when done gripping
+				timeCycles = 0;//reset time
+				while(grip){//while gripping, increment time count on 100Hz scheduler
 					if(tot_overflow > 2){
 						if(timeCycles < timeToCloseGrip){
-							timeCycles++;
+							timeCycles++;//increment value itself
 							printf(". . .\r");
-				}
-						else grip = 0;
-				}
+							}
+						else grip = 0;//once delay is over, cascade into case 4
+					}
 				}
 			}
 			break;
@@ -153,79 +130,70 @@ int main(void){
 		case 4://Pinch it up, using servo controls through coprocessor
 			printf("Gripped\r\n");
 
-			char notThere = 1;
-			while(notThere){
-				if(tot_overflow > 2){
-				gotoAngles(45,-45);
-				if((potAngle(2) == 45) && (potAngle(3) == -45)){
-					notThere=0;
-				}
+			char notThere = 1;//declare variable to wait for movement to position
+			while(notThere){//while the variable is true, the arm is not there
+				if(tot_overflow > 2){//PID at 100 Hz
+				gotoAngles(45,-45);//move to weighing position
+					if((potAngle(2) == 45) && (potAngle(3) == -45)){//check if there
+						notThere=0;//if it IS there, the it is not NOT there 8^)
+					}
 				}
 			}
-			stopMotors();
+			stopMotors();//stop motors to initiate weighing.
 			printf("Weigh Block\r\n");
 
-			state = 5;
+			state = 5;//Move to weighing stage
 			break;
 
 		case 5://Lift and weigh
 			printf("Weighing...\r\n");
-			//driveLink(1,1024);
-			//delay some more time
-			timeCycles = 0;
-			while(timeCycles < timeToWeigh){
-				if(tot_overflow>2){timeCycles++;tot_overflow = 0;}
-				}
+			timeCycles = 0;//Set time to zero to count up from.
 
+			while(timeCycles < timeToWeigh){//count time while less that timeToWeigh
+				if(tot_overflow>2){timeCycles++;tot_overflow = 0;}//delay increment
+			}
 
 			printf("Weighing Complete\r\n");
-			state = 6;
+			state = 6;//Move to sorting stage
 			break;
 
 		case 6://decide sorting
-			if(potAngle(2) < 40){state = 7;}//heavy block if hasn't moved
-			else {state = 8;} //it's a light block if it has moved
+			if(potAngle(2) < 40){state = 7;}//Heavy block if arm has not moved.
+			else {state = 8;} //If arm has not moved past threshold, light block.
 			break;
 
-		case 7://position of Light Block
+		case 7://Position of Heavy Block
 			printf("Heavy Block\r\n");
+			timeCycles = 0;//Set time counter to 0.
 
-			timeCycles = 0;
-
-			while(timeCycles < timeToGo){
-			if(tot_overflow>2){gotoAngles(0,0);timeCycles++;}
+			while(timeCycles < timeToGo){//While moving to position
+			if(tot_overflow>2){gotoAngles(0,0);timeCycles++;}//Operate PID at 100Hz
 			}
+			//And then move to state 9 to drop block.
 			state = 9;
 			break;
 
-		case 8://Position of Heavy Block
+		case 8://Position of Light Block
 			printf("Light BLock\r\n");
-			state = 9;
+			state = 9;//Move to dropping case.
 			break;
 
-		case 9://drop the block
-			setServo(0,0);//find out commands through testing
-			stopMotors();
+		case 9://Drop the block
+			setServo(0,0);//open the grippers
+			stopMotors();//stop function of the motors
 			printf("Drop\r\n");
 			printf("\r\n");
-			state = 0;
+			state = 0;//Call first case, repeat function
 			break;
 
-		default:
-			state = 0;
+		default://put the arm into a safe state
+			state = 0;//return to the known path
+			stopMotors();//stop motors from moving
+			setServo(0,0);//open gripper
 			printf("\r\n");
 			printf("default");
 			printf("\r\n");
 		}
-	//	printf("%6.3f, %6.3f\n\r", getAngles(8, 3)[0], getAngles(8, 3)[1]);
-
-//		if(tot_overflow>2) //2 sets the sample rate to 109Hz
-//		{//make a scheduler, check if overflowed
-//		tot_overflow = 0; //reset scheduler
-//			//printf("%d\n\r",getRange());
-//
-//		}
 	}
-
 return 0;
 }
